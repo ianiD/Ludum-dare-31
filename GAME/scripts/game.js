6da5e3af3b20edfd49990fa463e8071defc8c672
ianiD <123ioandragomir123@gmail.com>
2014-12-06 18:49:31 +0200

    Rendering zombies in the correct order

--- GAME/scripts/game.js
+++ GAME/scripts/game.js
@@ -15,7 +15,9 @@ var G = {
 	lastTime: null, time: 0.0,
 	LMB: false, MMB: false, RMB: false,
 	keys: new Array(256),
-	player: {x: 320, y: 240, health: 100, orientation: 'N', walking: false},
+	entities: new Array(501),
+	player: {x: 320, y: 240, hp: 5, orientetion: 'N', walking: false, faction: "N"},
+	zombies: new Array(500),
 	mousePos: {x: 0, y: 0},
 	init: function() {
 		G.canv = document.getElementById("gameCanvas");
@@ -33,6 +35,13 @@ var G = {
 		G.canv.addEventListener('mousedown', function(evt) {G.getMouseButtons(evt, true);}, false);
 		G.canv.addEventListener('mouseup', function(evt) {G.getMouseButtons(evt, false);}, false);
 		document.getElementById("FTG_sound").play();
+		for(i=0;i<500;i++)
+			G.entities[i] = G.zombies[i] = {hp:6,								//zombies
+							x:Math.round(Math.random()*800),
+							y:Math.round(Math.random()*600),
+							active:true, orientetion: 'N', walking: false,
+							drops:Math.round(Math.random()*20), faction:"Z"};
+		G.entities[500] = G.player;
 		
 	},
 	clearCanvas: function(){G.ctx.fillStyle="#000";G.ctx.fillRect(0,0,G.width,G.height);},
@@ -45,27 +54,38 @@ var G = {
 		var arrow_keys   = true,
 			qwerty       = true,
 			azerty       = false;
-		G.player.walking = false;
+		for(i=0;i<500;i++)
+			G.entities[i].walking = false;
 		if(G.gameState === "PLAY"){
-			if((G.keys[37]===true&&arrow_keys)||(G.keys[65]&&qwerty)||(G.keys[81]&&azerty)){	//	☃up☃
-				G.player.x-=dt * G.playerSpeed;
-				G.player.orientation = 'W';
-				G.player.walking=true;
+			for(i=0;i<501;i++)
+				G.entities[i].walking=false;
+			if((G.keys[37]===true&&arrow_keys)||(G.keys[65]&&qwerty)||(G.keys[81]&&azerty)){	//	☃left☃
+				for(i=0;i<501;i++){
+					G.entities[i].x-=dt * G.playerSpeed;
+					G.entities[i].orientation = 'E';
+					G.entities[i].walking=true;
+				}
 			}
-			if((G.keys[38]===true&&arrow_keys)||(G.keys[87]&&qwerty)||(G.keys[90]&&azerty)){	//	☃left☃
-				G.player.y-=dt * G.playerSpeed;
-				G.player.orientation = 'S';
-				G.player.walking=true;
+			if((G.keys[38]===true&&arrow_keys)||(G.keys[87]&&qwerty)||(G.keys[90]&&azerty)){	//	☃up☃
+				for(i=0;i<501;i++){
+					G.entities[i].y-=dt * G.playerSpeed;
+					G.entities[i].orientation = 'S';
+					G.entities[i].walking=true;
+				}
 			}
 			if((G.keys[39]===true&&arrow_keys)||(G.keys[68]&&qwerty)||(G.keys[68]&&azerty)){	//	☃right☃
-				G.player.x+=dt * G.playerSpeed;
-				G.player.orientation = 'E';
-				G.player.walking=true;
+				for(i=0;i<501;i++){
+					G.entities[i].x+=dt * G.playerSpeed;
+					G.entities[i].orientation = 'W';
+					G.entities[i].walking=true;
+				}
 			}
 			if((G.keys[40]===true&&arrow_keys)||(G.keys[83]&&qwerty)||(G.keys[83]&&azerty)){	//	☃down☃
-				G.player.y+=dt * G.playerSpeed;
-				G.player.orientation = 'N';
-				G.player.walking=true;
+				for(i=0;i<501;i++){
+					G.entities[i].y+=dt * G.playerSpeed;
+					G.entities[i].orientation = 'N';
+					G.entities[i].walking=true;
+				}
 			}
 		}
 		if(G.gameState === "LOGOS")
@@ -90,6 +110,17 @@ var G = {
 				G.LMB=false;
 			}
 		}
+		if(G.gameState === "LOST") {
+			G.setCookie("LD31_FTG_wave", 1, 10000);
+			if(G.LMB)
+				if(G.mousePos.x>0&&G.mousePos.y>0&&G.mousePos.x<0&&G.mousePos.y<0)
+					G.gameState = "MENU";
+		}
+		if(G.gameState === "WON") {
+			if(G.LMB)
+				if(G.mousePos.x>0&&G.mousePos.y>0&&G.mousePos.x<0&&G.mousePos.y<0)
+					G.gameState = "MENU";
+		}
 	},
 	draw: function() {
 		G.clearCanvas();
@@ -108,22 +139,34 @@ var G = {
 					G.ctx.drawImage(helpfocus, 0, 0);
 				break;
 			case "PLAY":
-				var playerSprite = document.getElementById("PlayerSprites");
-				if(!G.player.walking) {
-					switch(G.player.orientation) {
-						case 'N': G.ctx.drawImage(playerSprite, 0,   0, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'S': G.ctx.drawImage(playerSprite, 0, 100, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'E': G.ctx.drawImage(playerSprite, 0, 200, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'W': G.ctx.drawImage(playerSprite, 0, 300, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-					}
-				} else {
-					switch(G.player.orientation) {
-						case 'N': G.ctx.drawImage(playerSprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50,   0, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'S': G.ctx.drawImage(playerSprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 100, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'E': G.ctx.drawImage(playerSprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 200, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
-						case 'W': G.ctx.drawImage(playerSprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 300, 50, 100, G.player.x - 10, G.player.y - 20, 20, 40);break;
+				var playerSprite = document.getElementById("PlayerSprites"),
+					zombieSprite = document.getElementById("ZombieSprites"),
+					sprite;
+				G.entities.sort(function(a, b){return a.y-b.y;});
+				
+				for(i=0;i<501;i++){
+					sprite = playerSprite;
+					if(G.entities[i].faction==="Z")
+						sprite = zombieSprite;
+					
+					if(!G.entities[i].walking) {
+						switch(G.entities[i].orientation) {
+							case 'N': G.ctx.drawImage(sprite, 0,   0, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'S': G.ctx.drawImage(sprite, 0, 100, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'E': G.ctx.drawImage(sprite, 0, 200, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'W': G.ctx.drawImage(sprite, 0, 300, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+						}
+					} else {
+						switch(G.entities[i].orientation) {
+							case 'N': G.ctx.drawImage(sprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50,   0, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'S': G.ctx.drawImage(sprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 100, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'E': G.ctx.drawImage(sprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 200, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+							case 'W': G.ctx.drawImage(sprite, 50 + Math.floor(G.time * G.animationSpeed) % 2 * 50, 300, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
+						}
 					}
 				}
+				
+				
 				break;
 			case "HELP":
 				var help = document.getElementById("HelpImage"),
@@ -156,6 +199,22 @@ var G = {
 		var button = evt.which || evt.button;
 		if(button==1)
 			G.LMB=down;
+	},
+	setCookie: function(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+d.toUTCString();
+		document.cookie = cname + "=" + cvalue + "; " + expires;
+	},
+	getCookie: function(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+		}
+		return "";
 	}
 };
 

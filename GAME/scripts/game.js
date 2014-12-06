f98c2266042e457bda9d3b9e615702c52bd3e728
ianiD <123ioandragomir123@gmail.com>
2014-12-06 06:20:28 +0200

    code

--- GAME/scripts/game.js
+++ GAME/scripts/game.js
@@ -1,7 +1,33 @@
-var GAME = {
+var G = {
+	width: 640,
+	height: 480,
 	ctx: null,
+	canv: null,
+	gameState: "LOGOS",
+	interval: null,
+	lastTime: null,
 	init: function() {
-		ctx = document.getElementById("gameCanvas").getContext("2d");
-		ctx.fillRect(0,0,1000,1000);
+		G.canv = document.getElementById("gameCanvas");
+		G.ctx = G.canv.getContext("2d");
+		G.canv.width = G.width; G.canv.height = G.height;
+		interval = setInterval(G.tick, 0);
+		lastTime = Date.now();
+	},
+	clearCanvas: function(){
+		G.ctx.fillColor="#000";
+		G.ctx.fillRect(0,0,G.width,G.height);
+	},
+	tick: function() {
+		var now = Date.now();
+		var delta = G.lastTime - now;
+		G.lastTime = now;
+		G.update(delta);
+		G.draw();
+	},
+	update: function(dt) {
+	
+	},
+	draw: function() {
+		G.clearCanvas();
 	}
-}
+};

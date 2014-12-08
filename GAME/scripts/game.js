9727f224bea04198f5c63cbc09fedf8e3bafc614
ianiD <123ioandragomir123@gmail.com>
2014-12-08 20:57:03 +0200

    ooh, a lot of stuff

--- GAME/scripts/game.js
+++ GAME/scripts/game.js
@@ -11,17 +11,23 @@ var G = {
 	LMB: false, MMB: false, RMB: false,
 	keys: new Array(256),
 	entities: new Array(501),
-	projectiles: new Array(201),
-	player: {x: 320, y: 240, hp: 5, orientation: 'N', walking: false, faction: 0, active:true, animationSpeed: 0.01, walkSpeed: 0.1, tX: 320, tY: 240, dX: 0, dY: 0},
+	projectile: {dir: 0, active: false, x: 0, y: 0},
+	playerCooldown: 1000,
+	player: {x: 320, y: 240, hp: 5, orientation: 'N', walking: false, faction: 0, active:true, animationSpeed: 0.01, walkSpeed: 0.1, tX: 320, tY: 240, dX: 0, dY: 0, weapon: "FIST"},
 	deltaRotX: [+1, +1,  0, -1, -1, -1,  0, +1],
 	deltaRotY: [ 0, +1, +1, +1,  0, -1, -1, -1],
+	gunrot: 0,
+	score: 0,
+	weapon: 0, //0 is spoon, 1 is gun
+	mouseAngle: 0,
+	zombieCount: 0,
 	light:true,
 	tiles: new Array(20),
 	zombies: new Array(500),
+	newWepTime: 0,
 	mousePos: {x: 0, y: 0},
-
 	spritesheet: null,
-
+	lagMagicNumber: 1000,	//we want to have 10<deltaTime<50 most of the time, so we count form 0 to lagMagicNumber every tick to slow down the game a bit or a lot
 	init: function() {
 		G.canv = document.getElementById("gameCanvas");
 		G.ctx = G.canv.getContext("2d");
@@ -31,58 +37,100 @@ var G = {
 		G.lastTime = Date.now();
 		for(i=0; i<256; i++)
 			G.keys[i] = false;
-		document.onkeydown = function(e) {e = e || event;G.keys[e.keyCode] = true; }
-		document.onkeyup = function(e)   {e = e || event;G.keys[e.keyCode] = false;}
+		document.onkeydown = function(e) {e = e || event;G.keys[e.keyCode] = true;}
+		document.onkeyup = function(e)   {e = e || event;G.keys[e.keyCode] = false; if(e.keyCode==70)G.light=!G.light;}
 		G.canv.addEventListener('mousemove', function(evt) {G.mousePos = G.getMousePos(G.canv, evt);}, false);
 		G.canv.addEventListener('mousedown', function(evt) {G.getMouseButtons(evt, true);}, false);
 		G.canv.addEventListener('mouseup', function(evt) {G.getMouseButtons(evt, false);}, false);
 		document.getElementById("FTG_sound").play();
 		for(i=0;i<500;i++){
-			G.entities[i] = G.zombies[i] = {hp: 6,								//zombies
-							x:Math.round(Math.random()*800), tX:Math.round(Math.random()*800), 
-							y:Math.round(Math.random()*600), tY:Math.round(Math.random()*600), 
-							active:false, orientation: 'N', walking: false,			//TODO: drops
-							drops:Math.round(Math.random()*20), faction: 1,
-							walkSpeed: (Math.random()*0.075 + 0.025) , animationSpeed: 1};
-			G.entities[i].animationSpeed = G.entities[i].walkSpeed / 10;
+			G.zombies[i] = {hp: 5,								//zombies
+							x:Math.round(Math.random()*700 + 50), tX:0, 
+							y:Math.round(Math.random()*500 + 50), tY:0, 
+							active:false, orientation: 'N', walking: false, faction: 1, cooldown: 1000,
+							walkSpeed: (Math.random()*0.075 + 0.025) , animationSpeed: 0.01};
 			G.zombies[i].animationSpeed = G.zombies[i].walkSpeed / 10;
 		}
 		G.tiles =  [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
 					[3, 4, 5, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 					[3, 6, 8, 6, 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 5, 4, 5, 4, 5, 4, 5, 3],
 					[3, 4, 5, 4, 5, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 6, 8, 6, 8, 6, 8, 6, 8, 3],
-					[3, 6, 8, 6, 8, 2, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 4, 5, 4, 5, 4, 5, 4, 5, 3],
-					[3, 1, 1, 2, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 8, 6, 8, 6, 8, 6, 8, 3],
+					[3, 6, 8, 6, 8, 1, 1, 1, 1, 2, 2, 1, 1, 1, 2, 1, 4, 5, 4, 5, 4, 5, 4, 5, 3],
+					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 8, 6, 8, 6, 8, 6, 8, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 5, 4, 5, 4, 5, 4, 5, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 1, 1, 6, 8, 6, 8, 6, 8, 6, 8, 3],
-					[3, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 1, 2, 2, 1, 1, 4, 5, 4, 5, 1, 1, 4, 5, 3],
-					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 6, 8, 6, 8, 6, 8, 3, 3, 3],
-					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 5, 3, 3, 3, 3, 3],
+					[3, 2, 1, 2, 1, 1, 1, 1, 2, 2, 2, 1, 2, 2, 1, 1, 4, 5, 4, 5, 1, 2, 4, 5, 3],
+					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 6, 8, 6, 8, 6, 8, 6, 8, 3],
+					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 5, 1, 1, 1, 1, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
-					[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-					[3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3],
+					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 3],
+					[3, 2, 1, 1, 1, 2, 1, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 2, 2, 1, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 					[3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3],
 					[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]];
-		G.entities[500] = G.player;
 		G.spritesheet = document.getElementById("Sprsheet");
 	},
 	clearCanvas: function(){G.ctx.fillStyle="#000";G.ctx.fillRect(0,0,G.width,G.height);},
 	tick: function() {
 		var now = Date.now();var delta = now - G.lastTime;G.lastTime = now;
 		G.update(delta);G.draw();
-		for(i=0;i<1000;i++){}
+		for(i=0;i<G.lagMagicNumber;i++){}
 	},
 	update: function(dt) {
 		G.time+=dt;
-		document.getElementById("time").innerHTML="Debug info:<br>Delta time: "+dt+"<br>Elapsed time: "+G.time+"ms";
+		if(dt<10)
+			G.lagMagicNumber = Math.round(G.lagMagicNumber * 1.5);
+		if(dt>50)
+			G.lagMagicNumber = Math.round(G.lagMagicNumber / 1.5);
+		if(G.lagMagicNumber === 0)
+			G.lagMagicNumber = 1;
+		G.debug("DletaTime: " + dt, 0);
+		G.debug("Dt magic number: " + G.lagMagicNumber, 1);
+		G.debug("Time: " + G.time, 2);
 		var arrow_keys   = true,
 			qwerty       = true,
 			azerty       = true;
 		switch(G.gameState) {
 			case "PLAY":
+				G.playerCooldown -= dt;
+				for(i=0;i<500;i++)
+					if(G.zombies[i].active)
+						G.zombies[i].cooldown -= dt;
+				if(G.projectile.cooldown<990)
+					G.projectile.active=false;
+				for(i=0;i<500;i++)
+					if(G.zombies[i].hp<1&&G.zombies[i].active){
+						G.zombies[i].active = false;
+						G.score++;
+					}
+				if(G.player.hp < 1){
+					G.gameState = "LOST";
+					document.getElementById("desc").play();
+				}
+				if(G.score > 499){
+					G.gameState = "WON";
+					document.getElementById("desc").play();
+				}
+				if(G.score === 10 && G.weapon === 0){
+					G.weapon = 1;
+					G.newWepTime = G.time + 1000;
+					document.getElementById("nweap").play();
+				}
+				if(Math.random()<0.005)
+					G.zombies[++G.zombieCount].active = true;
+				G.mouseAngle = Math.atan2(G.player.y-G.mousePos.y, G.player.x-G.mousePos.x);
+
+				var dmin = 10;
+				for(j=0;j<16;j++){
+					if(dmin>Math.abs(G.mouseAngle - (j-8)*Math.PI/8)){
+						dmin = Math.abs(G.mouseAngle - (j-8)*Math.PI/8);
+						G.gunrot = Math.round(j/2);
+					}
+				}
+				if(G.gunrot===8)
+					G.gunrot=0;
 				if(G.keys[27]) {
 					G.setCookie("LD31_FTG_wave",G.curr_wave,1000);
 					G.gameState = "MENU";
@@ -100,6 +148,9 @@ var G = {
 				if((G.keys[40]===true&&arrow_keys)||(G.keys[83]&&qwerty)||(G.keys[83]&&azerty)) {	//	down
 					G.player.dY = +dt;
 				}
+				if(G.LMB) {
+					G.attack();
+				}
 				G.player.tX=G.player.x+G.player.dX*G.player.walkSpeed;
 				G.player.tY=G.player.y+G.player.dY*G.player.walkSpeed;
 				G.ai(dt);
@@ -110,10 +161,19 @@ var G = {
 						document.getElementById("TITLE_sound").play();
 					}
 					break;
+				break;
 			case "MENU":
 				if(G.LMB){
-					if(G.mousePos.x>540&&G.mousePos.y>300&&G.mousePos.x<750&&G.mousePos.y<390)
+					if(G.mousePos.x>540&&G.mousePos.y>300&&G.mousePos.x<750&&G.mousePos.y<390){
 						G.gameState = "PLAY";
+						G.score = 0;
+						G.weapon = 0;
+						G.player.hp = 5;
+						for(i=0;i<500;i++){
+							G.zombies[i].active=false;
+							G.zombies[i].hp=5;
+						}
+					}
 					if(G.mousePos.x>420&&G.mousePos.y>390&&G.mousePos.x<800&&G.mousePos.y<480)
 						G.gameState = "CREDITS";
 					if(G.mousePos.x>420&&G.mousePos.y>480&&G.mousePos.x<800&&G.mousePos.y<580)
@@ -129,15 +189,11 @@ var G = {
 				}
 				break;
 			case "LOST":
-				G.setCookie("LD31_FTG_wave", 1, 10000);
-				if(G.LMB)
-					if(G.mousePos.x>0&&G.mousePos.y>0&&G.mousePos.x<0&&G.mousePos.y<0)
-						G.gameState = "MENU";
-				break;
 			case "WON":
-				if(G.LMB)
-					if(G.mousePos.x>0&&G.mousePos.y>0&&G.mousePos.x<0&&G.mousePos.y<0)
-						G.gameState = "MENU";
+				G.setCookie("LD31_FTG_wave", 1, 10000);
+			case "CREDITS":
+				if(G.keys[27])
+					G.gameState = "MENU";
 				break;
 		}
 	},
@@ -158,28 +214,25 @@ var G = {
 					G.ctx.drawImage(helpfocus, 0, 0);
 				break;
 			case "PLAY":
-				var mouseAngle, gunrot, gunspr = [document.getElementById("Gun0"),
-												document.getElementById("Gun1"),
-												document.getElementById("Gun2"),
-												document.getElementById("Gun3"),
-												document.getElementById("Gun4"),
-												document.getElementById("Gun5"),
-												null,
-												document.getElementById("Gun7")],
+				var gunspr = [document.getElementById("Gun0"),
+							document.getElementById("Gun1"),
+							document.getElementById("Gun2"),
+							document.getElementById("Gun3"),
+							document.getElementById("Gun4"),
+							document.getElementById("Gun5"),
+							null,
+							document.getElementById("Gun7")],
+					spoonspr = [document.getElementById("Spn0"),
+							document.getElementById("Spn1"),
+							document.getElementById("Spn2"),
+							document.getElementById("Spn3"),
+							document.getElementById("Spn4"),
+							document.getElementById("Spn5"),
+							null,
+							document.getElementById("Spn7")],
 					HP = [document.getElementById("Hp0"),document.getElementById("Hp1"),document.getElementById("Hp2"),document.getElementById("Hp3"),document.getElementById("Hp4"),document.getElementById("Hp5")];
 				G.entities = G.zombies.concat([G.player]);
 				G.entities.sort(function(a, b){return a.y-b.y;});
-				mouseAngle = Math.atan2(G.player.y-G.mousePos.y, G.player.x-G.mousePos.x);
-				G.player.hp = Math.round(G.time / 100) % 6;
-				var dmin = 10;
-				for(j=0;j<16;j++){
-					if(dmin>Math.abs(mouseAngle - (j-8)*Math.PI/8)){
-						dmin = Math.abs(mouseAngle - (j-8)*Math.PI/8);
-						gunrot = Math.round(j/2);
-					}
-				}
-				if(gunrot===8)
-					gunrot=0;
 				for(i=0;i<19;i++) 
 					for(j=0;j<25;j++)
 						G.ctx.drawImage(G.spritesheet, 288 + (G.tiles[i][j] % 7) * 32, Math.floor(G.tiles[i][j] / 7) * 32, 32, 32, j*32, i*32, 32, 32);
@@ -200,26 +253,43 @@ var G = {
 							case 'E': G.ctx.drawImage(G.spritesheet, G.entities[i].faction*150 + 50 + Math.floor(G.time * G.entities[i].animationSpeed) % 2 * 50, 300, 50, 100, G.entities[i].x - 10, G.entities[i].y - 20, 20, 40);break;
 						}
 					}
-					if(gunrot!=6&&G.entities[i].faction==0)
-						G.ctx.drawImage(gunspr[gunrot], G.player.x - 100, G.player.y - 75, 200, 150);
+					if(G.weapon != 0){
+						if(G.playerCooldown>950){
+							G.ctx.lineWidth = 10;
+							G.ctx.strokeStyle = "#ff8";
+							G.ctx.beginPath();
+						    G.ctx.moveTo(G.player.x + 10*G.deltaRotX[G.gunrot], G.player.y + 10*G.deltaRotY[G.gunrot]);
+						    G.ctx.lineTo(G.player.x + 1000*G.deltaRotX[G.gunrot], G.player.y + 1000*G.deltaRotY[G.gunrot]);
+						    G.ctx.stroke();
+						}
+					}
+					if(G.gunrot!=6&&G.entities[i].faction==0)
+						if(G.weapon === 1)
+							G.ctx.drawImage(gunspr[G.gunrot], G.player.x - 100, G.player.y - 75, 200, 150);
+						else{
+							G.ctx.drawImage(spoonspr[G.gunrot], G.player.x - 100, G.player.y - 75, 200, 150);
+
+						}
 				}
-				//LIGHTMASK
-				//if(G.light)
 				var lightmask = document.getElementById("Light");
 				var nolight = document.getElementById("NoLight");
 				G.ctx.translate(G.player.x, G.player.y);
-				G.ctx.rotate(mouseAngle - Math.PI);
-				G.ctx.drawImage(lightmask, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
+				G.ctx.rotate(G.mouseAngle - Math.PI);
+				if(G.light)
+					G.ctx.drawImage(lightmask, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
 				G.ctx.scale(+1, -1);
-				G.ctx.drawImage(lightmask, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
+				if(G.light)
+					G.ctx.drawImage(lightmask, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
 				G.ctx.scale(-1, -1);
 				G.ctx.drawImage(nolight, 0, 0, 512, 1024, -0.5, -1024, 1024, 2048);
 				G.ctx.scale(-1, +1);
-				G.ctx.rotate(-mouseAngle + Math.PI);
+				if(!G.light)
+					G.ctx.drawImage(nolight, 0, 0, 512, 1024, -0.5, -1024, 1024, 2048);
+				G.ctx.rotate(-G.mouseAngle + Math.PI);
 				G.ctx.translate(-G.player.x, -G.player.y);
-				//high blocks
-				//GUI
 				G.ctx.drawImage(HP[G.player.hp], 323, 562);
+				if(G.newWepTime > G.time)
+					G.ctx.drawImage(document.getElementById("nwep"),0,0);
 				break;
 			case "HELP":
 				var help = document.getElementById("HelpImage"),
@@ -230,11 +300,16 @@ var G = {
 				break;
 			case "CREDITS":
 				var credits = document.getElementById("Credits");
-					G.ctx.drawImage(credits,0,0);
+				G.ctx.drawImage(credits,0,0);
 				break;
 			case "WON":
+				G.ctx.drawImage(document.getElementById("winImage"),0,0);
 				break;
 			case "LOST":
+				G.ctx.drawImage(document.getElementById("loseImage"),0,0);
+				G.ctx.font = 'bold 50pt Sans';
+				G.ctx.fillStyle = "#98170a";
+      			G.ctx.fillText(G.score, 320, 225);
 				break;
 			case "LOGOS":
 				var logo = document.getElementById("LogoImage");
@@ -250,6 +325,10 @@ var G = {
 		for(i=0;i<500;i++)	//for every zombie
 			if(Math.random() > 0.975 && G.zombies[i].active){
 				var player_distance = G.distance(G.zombies[i].x, G.zombies[i].y, G.player.x, G.player.y);
+				if(player_distance < 40 && G.zombies[i].cooldown<0) {
+					G.player.hp --;
+					G.zombies[i].cooldown = 1000;
+				}
 				G.zombies[i].tX = G.pointOnCircle(G.player, player_distance/2).x;
 				G.zombies[i].tY = G.pointOnCircle(G.player, player_distance/2).y;
 			}
@@ -265,24 +344,32 @@ var G = {
 	},
 	walk: function(who, dt) {
 		if(who.x<who.tX){
-			who.x+=dt*who.walkSpeed;
-			who.orientation = "E";
-			who.walking=true;
+			if(G.tiles[Math.round(who.y/32)][Math.round((who.x+dt*who.walkSpeed)/32)]!=3){
+				who.x+=dt*who.walkSpeed;
+				who.orientation = "E";
+				who.walking=true;
+			}
 		}
 		if(who.x>who.tX){
-			who.x-=dt*who.walkSpeed;
-			who.orientation = "W";
-			who.walking=true;
+			if(G.tiles[Math.round(who.y/32)][Math.round((who.x-dt*who.walkSpeed-16)/32)]!=3){
+				who.x-=dt*who.walkSpeed;
+				who.orientation = "W";
+				who.walking=true;
+			}
 		}
 		if(who.y<who.tY){
-			who.y+=dt*who.walkSpeed;
-			who.orientation = "S";
-			who.walking=true;
+			if(G.tiles[Math.round((who.y+dt*who.walkSpeed)/32)][Math.round(who.x/32)]!=3){
+				who.y+=dt*who.walkSpeed;
+				who.orientation = "S";
+				who.walking=true;
+			}
 		}
 		if(who.y>who.tY){
-			who.y-=dt*who.walkSpeed;
-			who.orientation = "N";
-			who.walking=true;
+			if(G.tiles[Math.round((who.y-dt*who.walkSpeed)/32)][Math.round(who.x/32)]!=3){
+				who.y-=dt*who.walkSpeed;
+				who.orientation = "N";
+				who.walking=true;
+			}
 		}
 	},
 	distance: function(x1, y1, x2, y2) {
@@ -313,6 +400,42 @@ var G = {
 			if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
 		}
 		return "";
+	},
+	debug: function(d, r){
+		document.getElementById("debug" + r).innerHTML = d;
+	},
+	lineRect: function(lX, lY, lD, rX, rY, rW, rH) {
+		var r1 = Math.atan2(lY-rY,		lX-rX),			//top-left
+			r2 = Math.atan2(lY-rY,		lX-rX-rW),		//top-right
+			r3 = Math.atan2(lY-rY-rH,	lX-rX),			//bottom-left
+			r4 = Math.atan2(lY-rY-rH,	lX-rX-rW),		//bottom_right
+			min = Math.min(r1, r2, r3, r4), minf,
+			max = Math.max(r1, r2, r3, r4), maxf;
+		minf=min;maxf=max;
+		if(min<-Math.PI/2 && max>+Math.PI/2){
+			minf = max;
+			maxf = min + Math.PI*2;
+		}
+		if(minf<lD && lD<maxf)
+			return true;
+		return false;
+	},
+	attack: function() {
+		if(G.playerCooldown>0)return;
+		G.playerCooldown = 1000;
+		document.getElementById("hit").play();
+		if(G.weapon===0)
+			for(i=0;i<500;i++)
+				if(G.zombies[i].active && G.zombies[i].hp > 0)
+					if(G.distance(G.player.x, G.player.y, G.zombies[i].x, G.zombies[i].y) < 64)
+						G.zombies[i].hp-=2;
+		else
+			for(i=0;i<500;i++)
+				if(G.zombies[i].active && G.zombies[i].hp > 0)
+					if(G.lineRect(G.player.x, G.player.y, G.gunrot * Math.PI / 4, G.zombies[i].x - 10, G.zombies[i].y - 20, 20, 40)){
+						G.zombies[i].hp -= 3;
+						console.log("hit");
+					}
 	}
 };
 
